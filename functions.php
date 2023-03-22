ff117bc78bd9553a194fe416a8bccf2a3d2f4435
Rob Thomas <robt84@gmail.com>
2023-03-22 18:19:23 -0500

    added role functionality

--- functions.php
+++ functions.php
@@ -100,7 +100,15 @@ function register_building_post_type() {
     $args = array(
         'labels' => array(
             'name' => 'Building',
-            'singular_name' => 'Building'
+            'singular_name' => 'Building',
+            'plural_name' => 'Buildings',
+            'add_new_item' => 'Add New Building',
+            'edit_item' => 'Edit Building',
+            'new_item' => 'New Building',
+            'view_item' => 'View Building',
+            'search_items' => 'Search Buildings',
+            'not_found' => 'No Buildings found',
+            'not_found_in_trash' => 'No Buildings found in Trash',
         ),
         'public' => true,
         'has_archive' => true,
@@ -114,7 +122,17 @@ function register_apartment_post_type() {
     $args = array(
         'labels' => array(
             'name' => 'Apartment',
-            'singular_name' => 'Apartment'
+            'singular_name' => 'Apartment',
+            'plural_name' => 'Apartments',
+            'parent_item_colon' => 'Building:',
+            'all_items' => 'All Apartments',
+            'add_new_item' => 'Add New Apartment',
+            'edit_item' => 'Edit Apartment',
+            'new_item' => 'New Apartment',
+            'view_item' => 'View Apartment',
+            'search_items' => 'Search Apartments',
+            'not_found' => 'No Apartments found',
+            'not_found_in_trash' => 'No Apartments found in Trash',
         ),
         'public' => true,
         'has_archive' => true,
@@ -126,3 +144,67 @@ function register_apartment_post_type() {
     register_post_type( 'apartment', $args );
 }
 add_action( 'init', 'register_apartment_post_type' );
+
+function add_author_support_to_posts() {
+    add_post_type_support( 'apartment', 'author' );
+}
+add_action( 'init', 'add_author_support_to_posts' );
+
+// Add support for the Agent role to be able to edit Apartments
+function restrict_agents_to_own_apartments($query) {
+    global $pagenow;
+
+    if ('edit.php' != $pagenow || !$query->is_admin || $query->get('post_type') != 'apartment') {
+        return $query;
+    }
+
+    $current_user = wp_get_current_user();
+
+    if (in_array('agent', $current_user->roles)) {
+        $query->set('author', $current_user->ID);
+    }
+
+    return $query;
+}
+add_action('pre_get_posts', 'restrict_agents_to_own_apartments');
+
+// Add support for the Agent role to be able to edit Buildings
+function restrict_agent_building_editing($caps, $cap, $user_id, $args) {
+    $current_user = get_userdata($user_id);
+
+    if (in_array('agent', $current_user->roles)) {
+        if ('edit_post' == $cap || 'delete_post' == $cap) {
+            $post = get_post($args[0]);
+
+            if ('building' == $post->post_type) {
+                $caps[] = 'do_not_allow';
+            }
+        } elseif ('publish_posts' == $cap && 'building' == get_post_type()) {
+            $caps[] = 'do_not_allow';
+        }
+    }
+
+    return $caps;
+}
+add_filter('map_meta_cap', 'restrict_agent_building_editing', 10, 4);
+
+
+// Add support for the Agent role to be able to edit Apartments
+function restrict_agent_add_apartment($allcaps, $cap, $args) {
+    if ('edit_posts' == $args[0] && 'apartment' == get_post_type()) {
+        $current_user = wp_get_current_user();
+
+        if (in_array('agent', $current_user->roles)) {
+            $allcaps['publish_posts'] = false;
+            $allcaps['edit_others_posts'] = false;
+            $allcaps['edit_published_posts'] = false;
+            $allcaps['delete_others_posts'] = false;
+            $allcaps['delete_published_posts'] = false;
+        }
+    }
+
+    return $allcaps;
+}
+add_filter('user_has_cap', 'restrict_agent_add_apartment', 10, 3);
+
+
